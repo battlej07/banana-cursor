db1a69bc36aaa0d43b90b489c1b422fb037521e7
Jamie Blättler <jamie.blaettler@proton.me>
2026-03-22 12:11:09 +0100

    feat: Add Everforest cursor

--- build.sh
+++ build.sh
@@ -34,6 +34,7 @@ names["Banana"]=$(with_version "The Banana")
 names["Banana-Green"]=$(with_version "The Green Banana")
 names["Banana-Blue"]=$(with_version "The Blue Banana")
 names["Banana-Red"]=$(with_version "The Red Banana")
+names["Banana-Everforest-Dark-Hard"]=$(with_version "The Everforest Dark Hard Banana")
 
 # Cleanup old builds
 rm -rf themes bin
@@ -43,7 +44,7 @@ for key in "${!names[@]}"; do
   comment="${names[$key]}"
   cfg=$(get_config_file key)
 
-  ctgen "configs/x.$cfg" -p x11 -d "bitmaps/$key" -n "$key" -c "$comment XCursors" &
+  ctgen "configs/x.$cfg" -s 8 12 16 24 32 48 64 96 128 192 256 -p x11 -d "bitmaps/$key" -n "$key" -c "$comment XCursors" &
   PID=$!
   wait $PID
 done
